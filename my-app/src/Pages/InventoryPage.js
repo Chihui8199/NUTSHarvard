0db1103c7814cb9b504def418797e5b32d8d8dbe
Brexton Ho <e0564446@u.nus.edu>
2022-10-15 17:43:42 -0500

    assets

--- my-app/src/Pages/InventoryPage.js
+++ my-app/src/Pages/InventoryPage.js
@@ -1,10 +1,43 @@
 import * as React from "react";
+import Button from '@material-ui/core/Button';
+import {Navigate} from 'react-router-dom';
+ 
+
+import chimchar from '../Images/chimchar.png';
+import piplup from '../Images/piplup.png';
+import turtwig from '../Images/turtwig.png';
+
+const style = {
+  margin: 15
+};
 
 const InventoryPage = () => {
+
+
+
   return (
-    <div>
-      
-    </div>
+    <div className="container pets">
+        <div className="pet-container">
+          <form>
+            <div className="venuesHeader">
+              <h1>Choose your pet!</h1>
+            </div>
+
+            <img src={piplup} name="Piplup" alt="Piplup" className="adopt-pet"  />
+            <img src={chimchar} name="Chimchar" alt="Chimchar"className="adopt-pet"  />
+            <img src={turtwig} name="Turtwig" alt="Turtwig"className="adopt-pet"/>
+            <br /><br />
+
+            <Button variant="Raised"
+              label="Select"
+              type="submit"
+              primary={true}
+              style={style}
+            />
+          </form>
+        </div>
+
+      </div>
   );
 };
 
